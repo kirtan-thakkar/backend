a2db1f27ec69dba19cef1f76ecb786c665c108ee
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-05-29 13:08:21 +0530

    Update boilerplate

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import WebSocket, {WebSockerServer} from "ws";
+import WebSocket, { WebSockerServer } from "ws";
 import http from "http";
 
-const server = http.createServer((req,res)=>{
-    console.log((new Date()) + " Received request for " + req.url);
-    res.end("Hi there from hhtp request mother fucker!")
-})
+const server = http.createServer((req, res) => {
+  console.log(new Date() + " Received request for " + req.url);
+  res.end("Hi there from hhtp request mother fucker!");
+});
 
-const wss = new WebSockerServer({server});
+const wss = new WebSockerServer({ server }); //websocker server
 
-wss.on("connection",function connection(ws){
-    ws.on("error",console.error);
+wss.on("connection", function connection(socket) {
+  socket.on("error", console.error);
 
-    ws.on("message",function message(data, isBinay){
-        if(client.readyState === WebSocket.OPEN){
-            client.send(data , {binary : isBinary});
-        }
+  socket.on("message", function message(data, isBinay) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
 
+  ws.send("Hi there from websocket server mother fucker!");
+});
 
-    })
-
-    ws.send("Hi there from websocket server mother fucker!");
-})
-
-server.listen(8080,()=>{
-    console.log((new Date()) + " Server is listening on port 8080");
-})
+server.listen(8080, () => {
+  console.log(new Date() + " Server is listening on port 8080");
+});
